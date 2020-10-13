41b5d7702a28e17c80dc8d912602c047843e8d0b
Administrator <a864511019@163.com>
2020-10-13 14:04:59 +0800

    API接口数据同步

--- micro-service-spring-admin/micro-service-admin-client/src/test/java/com/micro/service/adminclient/MicroServiceAdminClientApplicationTests.java
+++ micro-service-spring-admin/micro-service-admin-client/src/test/java/com/micro/service/adminclient/MicroServiceAdminClientApplicationTests.java
@@ -1,6 +1,6 @@
 package com.micro.service.adminclient;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
--- micro-service-spring-security/src/test/java/com/micro/service/springsecurity/MicroServiceSpringSecurityApplicationTests.java
+++ micro-service-spring-security/src/test/java/com/micro/service/springsecurity/MicroServiceSpringSecurityApplicationTests.java
@@ -1,6 +1,6 @@
 package com.micro.service.springsecurity;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
