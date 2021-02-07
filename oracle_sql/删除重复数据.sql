11a9f45c2d460e3ad5bbef0d2e98aa3c6a0a73b0
zhouxiaolejoe <a864511019@163.com>
2021-02-07 16:45:18 +0800

    删除重复数据

--- oracle_sql/删除重复数据.sql
+++ oracle_sql/删除重复数据.sql
@@ -10,3 +10,4 @@ DELETE FROM FASP_T_PUBMENU
 WHERE (guid,year,province) IN (SELECT guid,year,province FROM FASP_T_PUBMENU GROUP BY guid,year,province HAVING COUNT(guid) > 1)
 AND ROWID NOT IN
 (SELECT MIN(ROWID) FROM FASP_T_PUBMENU GROUP BY guid,year,province HAVING COUNT(*) > 1);
+---
