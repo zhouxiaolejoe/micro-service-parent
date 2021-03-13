2c5665f61f116a7821ef7fbdf9109e8783623cc6
zhouxiaolejoe <a864511019@163.com>
2021-03-13 15:31:42 +0800

    netty

---
+++ micro-service-netty/src/main/java/com/micro/service/netty/netty/TimeServerHandler.java
@@ -0,0 +1,22 @@
+package com.micro.service.netty.netty;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+public class TimeServerHandler extends ChannelInboundHandlerAdapter {
+
+        @Override
+        public void channelActive(final ChannelHandlerContext ctx) { // (1)
+            ChannelFuture f = ctx.writeAndFlush(new UnixTime());
+            f.addListener(ChannelFutureListener.CLOSE);
+        }
+
+        @Override
+        public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+            cause.printStackTrace();
+            ctx.close();
+        }
+    }
